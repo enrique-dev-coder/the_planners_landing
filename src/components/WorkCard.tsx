a83a96335af4d34bf6adba9eedc41e7ea2a48b62
Enrique <enrique_alvarado95@hotmail.com>
2023-11-02 13:23:38 -0600

    added decorations for work cards

--- src/components/WorkCard.tsx
+++ src/components/WorkCard.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Tira } from "./Icons";
 
 const WorkCard = ({
   imgLink,
@@ -20,9 +21,11 @@ const WorkCard = ({
           backgroundRepeat: "no-repeat",
           backgroundPosition: "center",
         }}
-        className="w-[390px] grayscale  rounded-3xl cursor- h-[540px] group-hover:grayscale-0 transition-all ease-in-out"
-      ></div>
-      {/* <Image width={390} height={540} src={imgLink} alt={boda} /> */}
+        className="w-[390px] group grayscale relative rounded-3xl cursor- h-[540px] group-hover:grayscale-0 transition-all ease-in-out"
+      >
+        <Tira className="grayscale-0! opacity-0  group-hover:opacity-100 transition-all absolute top-0 left-0  -translate-x-3 -translate-y-3" />
+        <Tira className="grayscale-0! opacity-0  group-hover:opacity-100 transition-all absolute  bottom-0 right-0  translate-x-3 translate-y-2" />
+      </div>
       <div className=" px-6 flex flex-col gap-2   mt-2 text-dark-main-text">
         <h3 className=" text-2xl font-bold">{boda}</h3>
         <p>{lugar}</p>
